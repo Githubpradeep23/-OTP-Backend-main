c93a058e5c486e743e4c1cef6ae3459b178df246
Githubpradeep23 <123489512+Githubpradeep23@users.noreply.github.com>
2023-08-12 16:52:42 +0530

    Update payment.js

--- models/payment.js
+++ models/payment.js
@@ -1,21 +1,17 @@
 const { mongoose, Schema } = require("mongoose");
-
 const paymentSchema = mongoose.Schema({
   voucher_id : [{ type: Schema.Types.ObjectId, ref: 'Voucher' }],
   userID: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  // packageId: [{ type: Schema.Types.ObjectId, ref: 'Package' }],
   service_id: [{ type: Schema.Types.ObjectId, ref: 'GYM_SERVICE' }],
   copuan_id: [{ type: Schema.Types.ObjectId, ref: 'Copuan'}],
   orderDetails: {
     type: Object,
     required: true,
   },
-
   duration: {
     type: String,
     default: null
   },
-
   price: {
     type: String,
     default: null
@@ -23,11 +19,8 @@ const paymentSchema = mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now(),
+
   },
 });
-
-
-
 const Payment = new mongoose.model("Payment", paymentSchema);
-
 module.exports = Payment;
